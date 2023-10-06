38450edd18a223adbe7d6e184315d8c7eb351038
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-10-06 10:49:20 +0530

    hooks in react

--- hooksreact/src/App.jsx
+++ hooksreact/src/App.jsx
@@ -4,20 +4,41 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [counter , setCounter] = useState(15); //here counter in the prameter is just like let counter only 
+  const [counter ,setCounter] = useState(0); //here counter in the prameter is just like let counter only 
 //setCounter is responsible to update the value of counter
+  const [counter1,getCounter] = useState(100);
+
+
 const addValue = () =>{
-  console.log("clicked", Math.random());
-  setCounter(counter + 1);
+if (counter >= 100){
+  console.log("cannot exceed");
 }
+else{
+setCounter(counter +1);
+
+}
+}
+
+const removeValue = () =>{
+  if (counter1 <= 0){
+    console.log("cannot go down");
+  }
+  else{
+  getCounter(counter1 - 1);
+  
+  }
+}
+
+
   return (
     <>
     <h1>Hooks in react</h1>
     <h2>counter value: {counter}</h2>
+    <h3>removing value {counter1}</h3>
      <button onClick={addValue}>Add Value{counter}</button>  
      {/*we are only passing the refrence as we want that when clicked only then it will execute
     <br></br> */}
-    <button>Remove Value</button>
+    <button onClick={removeValue}>Remove Value{counter1}</button>
 
     </>
   )
